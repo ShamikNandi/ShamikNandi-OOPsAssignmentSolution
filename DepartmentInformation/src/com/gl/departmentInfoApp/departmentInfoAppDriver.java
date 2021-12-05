9d0d5fae74492983daf1a630b1e85ed8538bd031
ShamikNandi <46488931+ShamikNandi@users.noreply.github.com>
2021-12-05 20:39:35 +0530

    Update departmentInfoAppDriver.java

--- DepartmentInformation/src/com/gl/departmentInfoApp/departmentInfoAppDriver.java
+++ DepartmentInformation/src/com/gl/departmentInfoApp/departmentInfoAppDriver.java
@@ -12,18 +12,18 @@ public class departmentInfoAppDriver {
 		HRDepartment objHR = new HRDepartment();
 		TechDepartment objTech = new TechDepartment();
 
-		System.out.println(" Welcome to" + objAdmin.departmentName());
+		System.out.println(" Welcome to " + objAdmin.departmentName());
 		System.out.println(objAdmin.getTodaysWork());
 		System.out.println(objAdmin.getWorkDeadline());
 		System.out.println(objAdmin.isTodayAHoliday() + "\n");
 
-		System.out.println(" Welcome to" + objHR.departmentName());
+		System.out.println(" Welcome to " + objHR.departmentName());
 		System.out.println(objHR.doActivity());
 		System.out.println(objHR.getTodaysWork());
 		System.out.println(objHR.getWorkDeadline());
 		System.out.println(objHR.isTodayAHoliday() + "\n");
 
-		System.out.println(" Welcome to" + objTech.departmentName());
+		System.out.println(" Welcome to  " + objTech.departmentName());
 		System.out.println(objTech.getTodaysWork());
 		System.out.println(objTech.getWorkDeadline());
 		System.out.println(objTech.getTechStackInformation());
